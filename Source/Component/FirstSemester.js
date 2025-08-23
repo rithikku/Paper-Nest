44a0b9096e646de2d00f62eb078b9fc07beacb14
Rithik <rithikmac13@gmail.com>
2025-08-23 23:32:57 +0530

    4th Commit

--- Source/Component/FirstSemester.js
+++ Source/Component/FirstSemester.js
@@ -3,7 +3,11 @@ const FirstSemester = () => {
         <div className="first-semester">
             <h2 className="first-sem-heading">FIRST SEMESTER PAPER</h2>
             <ul className="first-sem-list">
-                <li>Fundamental of Computers & Emerging Technologies</li>
+                 <li>
+                    <a href="https://www.aktuonline.com/papers/mca-1-sem-fundamental-of-computers-and-emerging-technologies-kca101-mar-2024.pdf" target="_blank" rel="noopener noreferrer">
+                        Fundamental of Computers & Emerging Technologies
+                    </a>
+                </li>
                 <li>Problem Solving using C</li>
                 <li>Principles of Management & Communication</li>
                 <li>Discrete Mathematics</li>
