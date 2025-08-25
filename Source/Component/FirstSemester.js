183c1816fb7fa1f0e32d387447a7e02ea0cf4261
Rithik <rithikmac13@gmail.com>
2025-08-25 12:02:19 +0530

    16th Commit

--- Source/Component/FirstSemester.js
+++ Source/Component/FirstSemester.js
@@ -1,43 +1,20 @@
-import { C_URL, CO_URL, CYBER_SECURITY_URL, DISCRETE_URL, FUNDAMENTAL_URL, PC_URL } from "../Utils/constants";
+import { firstSemesterSubjects } from "./FirstSemesterSubject";
 
 const FirstSemester = () => {
-    return (
-        <div className="first-semester">
-            <h2 className="first-sem-heading">FIRST SEMESTER PAPER</h2>
-            <ul className="first-sem-list">
-                 <li>
-                    <a href={FUNDAMENTAL_URL} target="_blank" rel="noopener noreferrer">
-                        BMC101 - Fundamental of Computers & Emerging Technologies
-                    </a>
-                </li>
-                 <li>
-                    <a href={C_URL} target="_blank" rel="noopener noreferrer">
-                        BMC102 - Problem Solving using C
-                    </a>
-                </li>
-                 <li>
-                    <a href={PC_URL} target="_blank" rel="noopener noreferrer">
-                        BMC103 - Principles of Management & Communication
-                    </a>
-                </li>
-                 <li>
-                    <a href={DISCRETE_URL} target="_blank" rel="noopener noreferrer">
-                        BMC104 - Discrete Mathematics
-                    </a>
-                </li>
-                 <li>
-                    <a href={CO_URL} target="_blank" rel="noopener noreferrer">
-                        BMC105 - Computer Organization & Architecture
-                    </a>
-                </li>
-                 <li>
-                    <a href={CYBER_SECURITY_URL} target="_blank" rel="noopener noreferrer">
-                        BMC106 - Cyber Security
-                    </a>
-                </li>
-            </ul>
-        </div>
-    );
-}
+  return (
+    <div className="first-semester">
+      <h2 className="first-sem-heading">FIRST SEMESTER PAPER</h2>
+      <ul className="first-sem-list">
+        {firstSemesterSubjects.map((subject, index) => (
+          <li key={index}>
+            <a href={subject.url} target="_blank" rel="noopener noreferrer">
+              {subject.code} - {subject.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default FirstSemester;
---
+++ Source/Component/FirstSemesterSubject.js
@@ -0,0 +1,10 @@
+import { C_URL, CO_URL, CYBER_SECURITY_URL, DISCRETE_URL, FUNDAMENTAL_URL, PC_URL } from "../Utils/constants";
+
+export const firstSemesterSubjects = [
+  { code: "BMC101", title: "Fundamental of Computers & Emerging Technologies", url: FUNDAMENTAL_URL },
+  { code: "BMC102", title: "Problem Solving using C", url: C_URL },
+  { code: "BMC103", title: "Principles of Management & Communication", url: PC_URL },
+  { code: "BMC104", title: "Discrete Mathematics", url: DISCRETE_URL },
+  { code: "BMC105", title: "Computer Organization & Architecture", url: CO_URL },
+  { code: "BMC106", title: "Cyber Security", url: CYBER_SECURITY_URL },
+];
