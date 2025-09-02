aed1b97fc919792b769a01b32b57a0ad10fe77e4
Rithik <rithikmac13@gmail.com>
2025-09-02 19:25:57 +0530

    23 Commit

--- Source/Utils/constants.js
+++ Source/Utils/constants.js
@@ -1,5 +1,5 @@
 export const ERROR_URL = "https://cdn.svgator.com/images/2024/04/electrocuted-caveman-animation-404-error-page.gif";
-export const LINKDIN_URL = "https://www.linkedin.com/in/rithik-kumar-%F0%9F%9A%80-547074231?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
+export const LINKDIN_URL = "https://www.linkedin.com/in/rithik-kumar-547074231?";
 export const GIRL_URL = "https://camo.githubusercontent.com/3e38d30f04e42688871c3de0a94852b9ec3c3b767e3ec2f9740fb144e462c47f/68747470733a2f2f63646e2e6472696262626c652e636f6d2f75736572732f323730343431342f73637265656e73686f74732f373436363930332f6d656469612f62303861623537363331366264343538326665663138396634373163643965352e676966";
 export const COMMING_URL = "https://tulikaeco.com/img/coming-soon.gif";
 
